b527e03d36c5d47d8d994e4e2fc84780e2441510
MehdiRtal <mehdirtal7@pm.me>
2026-02-01 18:54:55 +0100

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,19 @@
+set -g fish_greeting
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+fish_add_path $HOME/.krew/bin
+
 if status is-interactive
-    set -g fish_greeting
-    abbr -a ls lsd
-    set -gx PATH $PATH $HOME/.krew/bin
-    starship init fish | source
     fzf --fish | source
+    direnv hook fish | source
+    abbr -a k kubectl
+    abbr -a nano fresh
+end
+
+function add_newline --on-event fish_postexec
+   echo
+end
+
+function fish_prompt
+    echo (set_color --bold cyan)(prompt_pwd)(set_color normal)
+    printf '%s❯ %s' (set_color --bold green) (set_color normal)
 end
